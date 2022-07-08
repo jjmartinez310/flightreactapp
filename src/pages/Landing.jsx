f77d427226d99fdf1b6d4d5d238a0d3d05cd4713
jjmartinez310 <108151748+jjmartinez310@users.noreply.github.com>
2022-07-08 11:59:04 -0500

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,8 @@
-/*import { useState } from "react";*/
-
-
 export const Landing = () => {
-    //landing page with background image included. Couldn't get it to autosize so had to do a little brute forcing
+
     return (
-        
+        //Brute forced a wallpaper onto the page because CSS is tricky sometimes
         <div style={{ backgroundImage: `url(https://images.hdqwalls.com/wallpapers/airplane-minimalism-qhd.jpg)` }}>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
         <br/>
         <br/>
         <br/>
